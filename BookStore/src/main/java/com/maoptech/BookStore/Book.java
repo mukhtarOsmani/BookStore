f884a000750f5685bd3ee41afc1dfc3e66cfb501
Mukhtar Ahmad <mukhtar_payman@yahoo.com>
2020-12-22 07:53:39 -0500

    get ignore Configuration

---
+++ BookStore/src/main/java/com/maoptech/BookStore/Book.java
@@ -0,0 +1,45 @@
+package com.maoptech.BookStore;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "tbl_book")
+@Setter
+@Getter
+@ToString
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sku;
+	private String name;
+	private String description;
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+	@Column(name = "image_url")
+	private String imageUrl;
+	private boolean Active;
+	@Column(name = "units_in_stock")
+	private int UnitInStock;
+	@Column(name = "date_created")
+	private Date createdOn;
+	@Column(name = "last_update")
+	private Date updatedOn;
+	
+	
+	
+	
+}
